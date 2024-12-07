5f61e0f3163b78455c34ac4c8d4ea44fb3b8002f
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2024-12-07 14:58:34 +0200

    Update AES_Controller.v

--- src/AES_Controller.v
+++ src/AES_Controller.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Omar Ashraf AbdElmongy
 // 
 // Create Date: 12/07/2024 02:05:08 PM
 // Design Name: 
